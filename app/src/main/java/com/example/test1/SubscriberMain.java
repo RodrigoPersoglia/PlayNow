158ca7b0c114ff888c04722fb276643fbc4e4d98
Rodrigo Persoglia <71237928+RodrigoPersoglia@users.noreply.github.com>
2023-11-09 20:58:17 -0300

    suscripcion a evento

--- app/src/main/java/com/example/test1/SubscriberMain.java
+++ app/src/main/java/com/example/test1/SubscriberMain.java
@@ -2,28 +2,100 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SubscriberMain extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-    Button btn_logOut;
+public class SubscriberMain extends AppCompatActivity {
+    Button btn_logOut,btn_search,back_btn;
     FirebaseAuth mAuth;
+    private LinearLayout search_lay, subscriber_lay;
+    private Date selectedDate;
+    EditText dateCalendarEditText;
+    AutoCompleteTextView sportsEventAutoComplete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscriber_main);
+        StartComponents();
+    }
+
+    private void StartComponents() {
+        search_lay = findViewById(R.id.search_lay);
+        search_lay.setVisibility(View.GONE);
+        View search_event = getLayoutInflater().inflate(R.layout.search_event, null);
+        search_lay.addView(search_event);
+        subscriber_lay = findViewById(R.id.subscriber_lay);
         mAuth = FirebaseAuth.getInstance();
         btn_logOut = findViewById(R.id.btn_logout);
         btn_logOut.setOnClickListener(view -> LogOut());
+        btn_search = findViewById(R.id.search_btn);
+        btn_search.setOnClickListener(view -> Search());
+        back_btn = search_lay.findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(view -> Back());
     }
 
     private void LogOut(){
         mAuth.signOut();
         finish();
         startActivity(new Intent(SubscriberMain.this, MainActivity.class));
+    }
+
+    private void Search(){
+        subscriber_lay.setVisibility(View.GONE);
+        search_lay.setVisibility(View.VISIBLE);
+        dateCalendarEditText = search_lay.findViewById(R.id.date_CalendarEvent);
+
+        Button dateButton = search_lay.findViewById(R.id.image_calendar);
+        dateButton.setOnClickListener(viewCalendary -> {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(SubscriberMain.this, (datePickerView, year1, monthOfYear, dayOfMonth) -> {
+                calendar.set(year1, monthOfYear, dayOfMonth);
+                selectedDate = calendar.getTime();
+                SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+                String fechaFormateada = formatoFecha.format(selectedDate);
+                dateCalendarEditText.setText(fechaFormateada);
+            }, year, month, day);
+
+            datePickerDialog.show();
+        });
+
+        sportsEventAutoComplete = search_lay.findViewById(R.id.select_sportsEvent);
+
+        ArrayAdapter<String> autoCompleteAdapter = new ArrayAdapter<String>(this,
+                R.layout.custom_dropdown_item, R.id.text1, getResources().getStringArray(R.array.list_sports));
+
+        sportsEventAutoComplete.setAdapter(autoCompleteAdapter);
+
+        sportsEventAutoComplete.setOnClickListener(view1 -> {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(sportsEventAutoComplete.getWindowToken(), 0);
+            sportsEventAutoComplete.clearFocus();
+            sportsEventAutoComplete.showDropDown();
+        });
+    }
+
+    private void Back(){
+        subscriber_lay.setVisibility(View.VISIBLE);
+        search_lay.setVisibility(View.GONE);
     }
 }
